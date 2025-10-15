4fbef856f923930be939595677dc508cced50af2
PrestonWalk <prestonwalk123@gmail.com>
2025-10-15 10:03:37 -0700

    Basic setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TellyOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TellyOp.java
@@ -1,77 +1,25 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-    /*
-     * This file contains an example of a Linear "OpMode".
-     * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
-     * The names of OpModes appear on the menu of the FTC Driver Station.
-     * When a selection is made from the menu, the corresponding OpMode is executed.
-     *
-     * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
-     * This code will work with either a Mecanum-Drive or an X-Drive train.
-     * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
-     * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
-     *
-     * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
-     *
-     * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
-     * Each motion axis is controlled by one Joystick axis.
-     *
-     * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
-     * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
-     * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
-     *
-     * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
-     * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
-     * the direction of all 4 motors (see code below).
-     *
-     * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
-     * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
-     */
-
-    @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
-    @Disabled
+    @TeleOp(name="TellyOp 0.0.1")
+    //@Disabled
     public class TellyOp extends LinearOpMode {
 
         // Declare OpMode members for each of the 4 motors.
-        private ElapsedTime runtime = new ElapsedTime();
-        private DcMotor frontLeftDrive = null;
-        private DcMotor backLeftDrive = null;
-        private DcMotor frontRightDrive = null;
-        private DcMotor backRightDrive = null;
+        private final ElapsedTime runtime = new ElapsedTime();
 
         @Override
         public void runOpMode() {
 
             // Initialize the hardware variables. Note that the strings used here must correspond
             // to the names assigned during the robot configuration step on the DS or RC devices.
-            frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
-            backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
-            frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
-            backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+            DcMotor frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
+            DcMotor backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
+            DcMotor frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
+            DcMotor backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
 
-            // ########################################################################################
-            // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
-            // ########################################################################################
-            // Most robots need the motors on one side to be reversed to drive forward.
-            // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
-            // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
-            // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
-            // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
-            // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
-            // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
             frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
             backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
             frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -106,29 +54,13 @@ import com.qualcomm.robotcore.util.ElapsedTime;
                 max = Math.max(max, Math.abs(backLeftPower));
                 max = Math.max(max, Math.abs(backRightPower));
 
+                if (gamepad1.left_bumper) max *= 3.0; // in case we need more precise movement
                 if (max > 1.0) {
                     frontLeftPower  /= max;
                     frontRightPower /= max;
                     backLeftPower   /= max;
                     backRightPower  /= max;
                 }
-
-                // This is test code:
-                //
-                // Uncomment the following code to test your motor directions.
-                // Each button should make the corresponding motor run FORWARD.
-                //   1) First get all the motors to take to correct positions on the robot
-                //      by adjusting your Robot Configuration if necessary.
-                //   2) Then make sure they run in the correct direction by modifying the
-                //      the setDirection() calls above.
-                // Once the correct motors move in the correct direction re-comment this code.
-
-            /*
-            frontLeftPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            backLeftPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            frontRightPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            backRightPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
 
                 // Send calculated power to wheels
                 frontLeftDrive.setPower(frontLeftPower);
@@ -139,9 +71,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
                 // Show the elapsed game time and wheel power.
                 telemetry.addData("Status", "Run Time: " + runtime.toString());
                 telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
-                telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
+                telemetry.addData("Back left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
                 telemetry.update();
             }
-        }}
-
-
+        }
+    }
